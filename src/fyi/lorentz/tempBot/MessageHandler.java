501c9b7cd28cc486036957063eec8330f2b8ee61
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2017-12-04 21:01:48 -0600

    Add reactions to @here/@everyone mentions

--- src/fyi/lorentz/tempBot/MessageHandler.java
+++ src/fyi/lorentz/tempBot/MessageHandler.java
@@ -1,14 +1,26 @@
 package fyi.lorentz.tempBot;
 
 import fyi.lorentz.tempBot.service.TemperatureProcessor;
+
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.impl.obj.ReactionEmoji;
+import sx.blah.discord.handle.obj.IMessage;
 
 public class MessageHandler implements IListener<MessageReceivedEvent> {
+
+    private static final ReactionEmoji angryFace = ReactionEmoji.of("\uD83D\uDE20");
+    private static final ReactionEmoji thinkingFace = ReactionEmoji.of("\uD83E\uDD14");
 
     @Override
     public void
     handle(MessageReceivedEvent event) {
+        handleTemperatures(event);
+        handleBadMentions(event);
+    }
+
+    private void
+    handleTemperatures(MessageReceivedEvent event) {
         String inputText = event.getMessage().getFormattedContent();
 
         TemperatureProcessor tempProcessor = new TemperatureProcessor(inputText);
@@ -16,6 +28,19 @@ public class MessageHandler implements IListener<MessageReceivedEvent> {
 
         if(output.length() > 0) {
             event.getChannel().sendMessage(output);
+        }
+    }
+
+    private void
+    handleBadMentions(MessageReceivedEvent event) {
+        IMessage message = event.getMessage();
+
+        if(message.mentionsEveryone()) {
+            message.addReaction(angryFace);
+        }
+
+        if(message.mentionsHere()) {
+            message.addReaction(thinkingFace);
         }
     }
 }
