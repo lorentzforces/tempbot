d335c586b3b695566628245c0e990e1e05b9ad08
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2023-04-24 00:36:25 -0500

    add test verifying that leading punctuation doesn't break

--- src/test/java/tempbot/engine/TestFullTextMessageProcessor.java
+++ src/test/java/tempbot/engine/TestFullTextMessageProcessor.java
@@ -28,54 +28,54 @@ public class TestFullTextMessageProcessor {
 	@Test
 	public void
 	basicValuesAreParsed() {
-		var zeroCelsiusResults = processor.processMessage("0 C");
+		final var zeroCelsiusResults = processor.processMessage("0 C");
 		assertThat(zeroCelsiusResults, hasSize(1));
-		var zeroCelsius = getConvertedValuesFromResult(zeroCelsiusResults.get(0));
+		final var zeroCelsius = getConvertedValuesFromResult(zeroCelsiusResults.get(0));
 		assertDoublesEqual(0d, zeroCelsius.sourceValue().value());
 
-		var tenCelsiusResults = processor.processMessage("10 C");
+		final var tenCelsiusResults = processor.processMessage("10 C");
 		assertThat(tenCelsiusResults, hasSize(1));
-		var tenCelsius = getConvertedValuesFromResult(tenCelsiusResults.get(0));
+		final var tenCelsius = getConvertedValuesFromResult(tenCelsiusResults.get(0));
 		assertDoublesEqual(10d, tenCelsius.sourceValue().value());
 	}
 
 	@Test
 	public void
 	signsAreParsed() {
-		var positiveResults = processor.processMessage("40 C");
+		final var positiveResults = processor.processMessage("40 C");
 		assertThat(positiveResults, hasSize(1));
-		var positive = getConvertedValuesFromResult(positiveResults.get(0));
+		final var positive = getConvertedValuesFromResult(positiveResults.get(0));
 		assertDoublesEqual(40d, positive.sourceValue().value());
 
-		var negativeResults = processor.processMessage("-40 C");
+		final var negativeResults = processor.processMessage("-40 C");
 		assertThat(negativeResults, hasSize(1));
-		var negative = getConvertedValuesFromResult(negativeResults.get(0));
+		final var negative = getConvertedValuesFromResult(negativeResults.get(0));
 		assertDoublesEqual(-40d, negative.sourceValue().value());
 	}
 
 	@Test
 	public void
 	spacesAreOptional() {
-		var noSpaceResults = processor.processMessage("32F");
+		final var noSpaceResults = processor.processMessage("32F");
 		assertThat(noSpaceResults, hasSize(1));
-		var noSpace  = getConvertedValuesFromResult(noSpaceResults.get(0));
+		final var noSpace  = getConvertedValuesFromResult(noSpaceResults.get(0));
 		assertDoublesEqual(32d, noSpace.sourceValue().value());
 	}
 
 	@Test
 	public void
 	originalUnitIsNotIncludedInResults() {
-		var notOriginalUnitResults = processor.processMessage("32F");
+		final var notOriginalUnitResults = processor.processMessage("32F");
 		assertThat(notOriginalUnitResults, hasSize(1));
 
-		var notOriginalUnit = getConvertedValuesFromResult(notOriginalUnitResults.get(0));
+		final var notOriginalUnit = getConvertedValuesFromResult(notOriginalUnitResults.get(0));
 		assertThat(notOriginalUnit.values(), hasSize(1));
 	}
 
 	@Test
 	public void
 	onlyDefaultSourcesAreConverted() {
-		var conversions = processor.processMessage("32F 500K");
+		final var conversions = processor.processMessage("32F 500K");
 		assertThat(conversions, hasSize(2));
 
 		assertThat(
@@ -91,30 +91,42 @@ public class TestFullTextMessageProcessor {
 	@Test
 	public void
 	maxMinValuesAreRespected() {
-		var belowAbsoluteZeroResults = processor.processMessage("-300 C");
+		final var belowAbsoluteZeroResults = processor.processMessage("-300 C");
 		assertThat(belowAbsoluteZeroResults, hasSize(1));
-		var belowAbsoluteZero = getOutOfRangeFromResult(belowAbsoluteZeroResults.get(0));
+		final var belowAbsoluteZero = getOutOfRangeFromResult(belowAbsoluteZeroResults.get(0));
 		assertThat(belowAbsoluteZero.limitType(), is(LimitType.MINIMUM));
 	}
 
 	@Test
 	public void
 	conversionIsCorrect() {
-		var waterFreezingResults = processor.processMessage("32 F");
+		final var waterFreezingResults = processor.processMessage("32 F");
 		assertThat(waterFreezingResults, hasSize(1));
-		var celsius = getUnitValueFromResult(
+		final var celsius = getUnitValueFromResult(
 			getConvertedValuesFromResult(waterFreezingResults.get(0)),
 			"degrees Celsius"
 		);
 		assertDoublesEqual(0d, celsius);
 
-		var commonPointResults = processor.processMessage("-40 C");
+		final var commonPointResults = processor.processMessage("-40 C");
 		assertThat(commonPointResults, hasSize(1));
-		var fahrenheit = getUnitValueFromResult(
+		final var fahrenheit = getUnitValueFromResult(
 			getConvertedValuesFromResult(commonPointResults.get(0)),
 			"degrees Fahrenheit"
 		);
 		assertDoublesEqual(-40d, fahrenheit);
+	}
+
+	@Test
+	public void
+	conversionsWillWorkWithLeadingPunctuation() {
+		final var punctuationResults = processor.processMessage("(-32.8 F)");
+		assertThat(punctuationResults, hasSize(1));
+		final var convertedValue = getUnitValueFromResult(
+			getConvertedValuesFromResult(punctuationResults.get(0)),
+			"degrees Celsius"
+		);
+		assertDoublesEqual(-36d, convertedValue);
 	}
 
 //	@Test
@@ -130,22 +142,22 @@ public class TestFullTextMessageProcessor {
 	@Test
 	public void
 	multipleValuesAreConverted() {
-		var multipleResults = processor.processMessage("32 F and some text and 23 F");
+		final var multipleResults = processor.processMessage("32 F and some text and 23 F");
 		assertThat(multipleResults, hasSize(2));
 
 		// these should be strictly ordered to match their original values' appearances in the
 		// original message
-		var firstConversion = getConvertedValuesFromResult(multipleResults.get(0));
+		final var firstConversion = getConvertedValuesFromResult(multipleResults.get(0));
 		assertDoublesEqual(32d, firstConversion.sourceValue().value());
 
-		var secondConversion = getConvertedValuesFromResult(multipleResults.get(1));
+		final var secondConversion = getConvertedValuesFromResult(multipleResults.get(1));
 		assertDoublesEqual(23d, secondConversion.sourceValue().value());
 	}
 
 	@Test
 	public void
 	duplicateValuesAreConsolidated() {
-		var results = processor.processMessage("32F and 32F");
+		final var results = processor.processMessage("32F and 32F");
 		assertThat(results, hasSize(1));
 	}
 
@@ -153,16 +165,16 @@ public class TestFullTextMessageProcessor {
 	public void
 	maximumValuesProcessed() {
 		// assumes that Constants.MAX_CONVERSIONS is 10
-		var results = processor.processMessage("1F 2F 3F 4F 5F 6F 7F 8F 9F 10F 11F 12F");
+		final var results = processor.processMessage("1F 2F 3F 4F 5F 6F 7F 8F 9F 10F 11F 12F");
 		assertThat(results, hasSize(10));
 	}
 
 	@Test
 	public void
 	specificConversionSucceeds() {
-		var results = processor.processMessage("5C to Kelvin");
+		final var results = processor.processMessage("5C to Kelvin");
 		assertThat(results, hasSize(1));
-		var kelvin = getUnitValueFromResult(
+		final var kelvin = getUnitValueFromResult(
 			getConvertedValuesFromResult(results.get(0)),
 			"degrees Kelvin"
 		);
@@ -172,7 +184,7 @@ public class TestFullTextMessageProcessor {
 	@Test
 	public void
 	specificConversionWorksOnNonDefaultUnits() {
-		var specificResults = processor.processMessage("2K to C");
+		final var specificResults = processor.processMessage("2K to C");
 		assertThat(specificResults, hasSize(1));
 	}
 
